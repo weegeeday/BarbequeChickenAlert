7b20d395fa9dc9c621010b5210d26f30270fc273
weegeeday <38634611+weegeeday@users.noreply.github.com>
2026-04-14 14:22:04 -0400

    corndogcat version + routerworking

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import GameView from '../views/GameView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,16 +7,22 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: GameView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/home',
+      redirect: '/',
     },
+    {
+      path: '/menu',
+      name: 'menu',
+      component: () => import('../views/MenuListView.vue'),
+    },
+    {
+      path: '/corndog',
+      name: 'corndog',
+      component: () => import('../views/Corndog.vue'),
+    }
   ],
 })
 
